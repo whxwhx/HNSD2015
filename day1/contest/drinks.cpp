6767f6d7e4cecd740883b773ad723a69aeba808d
whxwhx <wu_hong_xun@qq.com>
2016-01-06 15:37:53 +0800

    Update drinks.cpp

--- day1/contest/drinks.cpp
+++ day1/contest/drinks.cpp
@@ -13,32 +13,33 @@ bool cmp (const drink &a, const drink &b){
 	return (a.a[0] < b.a[0]) || (a.a[0] == b.a[0] && a.a[1] < b.a[1]) || (a.a[0] == b.a[0] && a.a[1] == b.a[1] && a.a[2] < b.a[2]);
 }
 bool operator < (const drink &a, const drink &b){
-	return (a.a[1] < b.a[1]) || (a.a[1] == b.a[1] && a.a[2] < b.a[2]) || (a.a[1] == b.a[1] && a.a[2] == b.a[2] && a.a[3] < b.a[3]);
+	return (a.a[1] < b.a[1]) || (a.a[1] == b.a[1] && a.a[2] < b.a[2]) || (a.a[1] == b.a[1] && a.a[2] == b.a[2] && a.a[0] < b.a[0]);
 }
 int s[N];
 void add(int x, int d){
-	for(;x <= n; x += (-x) & x) s[x] += d;
+	for(;x <= (n << 1) + 1; x += (-x) & x) s[x] += d;
 }
 int sum(int x){
 	int ans = 0;
 	for(;x;x -= (-x) & x) ans += s[x];
 	return ans;
 }
+int f[N];
 void solve(int l, int r){
 	if (l == r) return;
 	int mid = (l + r) >> 1;
 	solve(l, mid), solve(mid + 1, r);
 	int t1 = l, t2 = mid + 1;
 	rep(i,l,r)
-		if (t1 <= mid && a[t1] < a[t2]){
+		if (t2 > r || (t1 <= mid && a[t1] < a[t2])){
 			add(a[t1].a[2], 1);
-			d[i] = a[t1++];
+			t[i] = a[t1++];
 		}else {
-			f[a[t2].num] = sum(a[t2].a[2]);
-			d[i] = a[t2++];
+			f[a[t2].num] += sum(a[t2].a[2]);
+			t[i] = a[t2++];
 		}
 	rep(i,l,mid) add(a[i].a[2], -1);
-	rep(i,l,r) a[i] = d[i];
+	rep(i,l,r) a[i] = t[i];
 }
 void calc(){
 	sort(a + 1, a + n + 1, cmp);
@@ -46,15 +47,15 @@ void calc(){
 }
 
 LL ans = 0;
+int F[N][8];
 int main(){
-	
 	scanf("%d",&n);
 	rep(i,1,n) {
 		d[i].num = i;
 		rep(j,0,2) scanf("%d",&d[i].a[j]);
 	}
 
-	rep(i,1,n) d[i] = a[i];
+	rep(i,1,n) a[i] = d[i];
 	calc();
 
 	//case 1
@@ -64,12 +65,21 @@ int main(){
 	//case 3
 	ans += 1LL * n * (n - 1) * (n - 2) / 6;
 		//case 3.1
-			rep(i,1,n) ans -= 1LL * f[i] * (f[i] - 1) / 2;
-		//case3.2
-			//case 3.2.1
-			
-			//case 3.2.2
-			
-	cout <<ans<<endl;
+			rep(i,1,n) ans += 1LL * f[i] * (f[i] - 1) / 2;
+		//case 3.2
+			rep(i,0,7){
+				rep(j,1,n) f[j] = 0;
+				rep(j,1,n) {
+					a[j].num = d[j].num;
+					rep(k,0,2) if (i >> k & 1) a[j].a[k] = -d[j].a[k] + n + 1; else a[j].a[k] = d[j].a[k] + n + 1;
+				}
+				calc();
+				rep(j,1,n) F[j][i] = f[j];
+			}
+			rep(i,1,n){
+				rep(j,0,7) rep(k,j,7) if ((j | k) == 7) {
+					if (j != k) ans -= 1LL * F[i][j] * F[i][k]; else ans -= 1LL * F[i][j] * (F[i][k] - 1) / 2;
+				}
+			}
 	return 0;
 }
